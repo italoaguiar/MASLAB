eb6ca3e514692e5e8c40fd2187b3967c35c610fd
Italo Almeida Aguiar <italo.almeida@outlook.com>
2022-03-04 22:20:31 -0300

    Ajuste no script de build

--- src/MASLAB/build.cmd
+++ src/MASLAB/build.cmd
@@ -11,6 +11,7 @@ dotnet zip -c Release -f netcoreapp3.1 -r win-x64
 
 echo Compilando versao do Linux
 dotnet publish -c Release -f netcoreapp3.1 -r linux-x64 --self-contained true
+dotnet deb -c Release -f netcoreapp3.1 -r linux-x64
 dotnet rpm -c Release -f netcoreapp3.1 -r linux-x64
 dotnet zip -c Release -f netcoreapp3.1 -r linux-x64
 dotnet tarball -c Release -f netcoreapp3.1 -r linux-x64
